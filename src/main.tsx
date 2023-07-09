64e325d201f352669f09123d6f6c8da713d96ad7
Clara <clarapachecodev@gmail.com>
2023-07-09 14:13:09 -0300

    Import GlobalStyle and use it around app component, inside ThemeProvider context component

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { App } from './App.js'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.js'
 
+import { GlobalStyle } from './styles/global.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App />
+      <GlobalStyle />
+        <App />
     </ThemeProvider>
   </React.StrictMode>,
 )
