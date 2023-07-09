e87088183eb0c9d4d3b515b337bf63abe9730c38
Clara <clarapachecodev@gmail.com>
2023-07-09 13:48:37 -0300

    Import ThemeProvider from styled-components and defaultTheme from themes folder and use it around <app> component passing defaultTheme as prop of ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.js'
 
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
