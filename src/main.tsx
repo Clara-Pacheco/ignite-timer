4672bcaf9f06ab827cc1d545ab9a4d161586805d
Clara <clarapachecodev@gmail.com>
2023-07-15 09:35:11 -0300

    Import BrowserRouter context component and component Router and use it in the construction of main page

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.js'
 
 import { GlobalStyle } from './styles/global.js'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
         <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )
