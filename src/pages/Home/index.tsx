9f86ac5edf686198faf8fb2a5c38b9e5329ba927
Clara <clarapachecodev@gmail.com>
2023-07-18 11:54:07 -0300

    Improve inputs by adding step, min and max props and datalist html element

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,12 +18,24 @@ export function Home() {
           <TaskInput
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           ></TaskInput>
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="- 00 +"
+            step={5}
+            min={5}
+            max={60}
           ></MinutesAmountInput>
           <span>minutos.</span>
         </FormContainer>
